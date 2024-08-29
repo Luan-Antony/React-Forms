3937f2292a71e3586b38745bbb65d3804a6c81e6
Luan Antony <fplpgluan@gmail.com>
2024-08-29 16:43:16 -0300

    Trocado as tag de input no nome da carta e do naipe para tags de select, facilita mostrando apenas as opções disponiveis

--- src/components/forms/form.jsx
+++ src/components/forms/form.jsx
@@ -12,7 +12,7 @@ const Form = ({ addCard }) => {
         const { target } = event;
         const { name } = target;
         const { value } = target;
-
+        //dentro do target vai ser pego do event, e o name e value vai ser pego desse target
         setInputs({
             ...inputs,
             [name]: value,
@@ -27,34 +27,46 @@ const Form = ({ addCard }) => {
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="image">Endereço da imagem da carta</label>
-                <input
-                    type="text"
-                    id="image"
-                    name="image"
-                    onChange={handleInputChange}
-                    value={inputs.image}
-                />
-            </div>
-            <div>
                 <label htmlFor="value">Nome da carta</label>
-                <input
-                    type="text"
+                <select
                     id="value"
                     name="value"
                     onChange={handleInputChange}
                     value={inputs.value}
-                />
+                >
+                    <option value="">Selecione</option>
+                    <option value="A">A</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="J">J</option>
+                    <option value="Q">Q</option>
+                    <option value="K">K</option>
+                </select>
             </div>
             <div>
-                <label htmlFor="suit">Naipe da carta</label>
-                <input
-                    type="text"
+                <label htmlFor="value">Nome da carta</label>
+
+            </div>
+            <div>
+                <select
                     id="suit"
                     name="suit"
                     onChange={handleInputChange}
                     value={inputs.suit}
-                />
+                >
+                    <option value="">Selecione</option>
+                    <option value="DIAMONDS">DIAMONDS</option>
+                    <option value="SPADES">SPADES</option>
+                    <option value="HEARTS">HEARTS</option>
+                    <option value="CLUBS">CLUBS</option>
+                </select>
             </div>
             <input type="submit" />
         </form>
